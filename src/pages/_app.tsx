6b8f054f79efcdd6819c4e9fa1cc2cea11b396ce
Mathews A <mathews.mw@gmail.com>
2022-12-06 21:33:32 -0400

    add mirage server and react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from 'next/app';
+import { makeServer } from '../services/mirage';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<ChakraProvider resetCSS theme={theme}>
-			<SidebarDrawerProvider>
-				<Component {...pageProps} />
-			</SidebarDrawerProvider>
-		</ChakraProvider>
-	);
+if (process.env.NODE_ENV === 'development') {
+	makeServer();
 }
 
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider resetCSS theme={theme}>
+				<SidebarDrawerProvider>
+					<Component {...pageProps} />
+				</SidebarDrawerProvider>
+			</ChakraProvider>
+
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	);
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,13 +1,47 @@
+import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
+
 import Header from '../../components/Header';
 import { SideBar } from '../../components/Sidebar';
-
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+import { Pagination } from '../../components/Pagination';
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-import { Pagination } from '../../components/Pagination';
-import Link from 'next/link';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+
+interface IUsersView {
+	id: number;
+	name: string;
+	email: string;
+	createdAt: string;
+}
 
 export default function UserList() {
+	const { data, isLoading, error } = useQuery(
+		['users'],
+		async () => {
+			const response = await fetch('http://localhost:3000/api/users');
+			const data = await response.json();
+
+			const users = data.users.map((user: IUsersView) => {
+				return {
+					id: user.id,
+					name: user.name,
+					email: user.email,
+					createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+						day: '2-digit',
+						month: 'long',
+						year: 'numeric',
+					}),
+				};
+			});
+
+			return users;
+		},
+		{
+			staleTime: 1000 * 5,
+		}
+	);
+
 	const isWideVersion = useBreakpointValue({
 		base: false,
 		lg: true,
@@ -33,59 +67,56 @@ export default function UserList() {
 						</Link>
 					</Flex>
 
-					<Table>
-						<Thead>
-							<Tr>
-								<Th px={['4', '4', '6']} color='gray.300' width='8'>
-									<Checkbox colorScheme='pink' />
-								</Th>
-								<Th>Usuário</Th>
-								<Th>Data de cadastro</Th>
-								<Th width='8'></Th>
-							</Tr>
-						</Thead>
-						<Tbody>
-							<Tr>
-								<Td px={['4', '4', '6']}>
-									<Checkbox colorScheme='pink' />
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight='bold'>Mathews Araujo</Text>
-										<Text fontSize='sm' color='gray.300'>
-											mathews.mw@gmail.com
-										</Text>
-									</Box>
-								</Td>
-								{isWideVersion && <Td>03 de Dezembro, 2022</Td>}
-								<Td>
-									<Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
-										Editar
-									</Button>
-								</Td>
-							</Tr>
-							<Tr>
-								<Td px={['4', '4', '6']}>
-									<Checkbox colorScheme='pink' />
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight='bold'>Mathews Araujo</Text>
-										<Text fontSize='sm' color='gray.300'>
-											mathews.mw@gmail.com
-										</Text>
-									</Box>
-								</Td>
-								{isWideVersion && <Td>03 de Dezembro, 2022</Td>}
-								<Td>
-									<Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
-										Editar
-									</Button>
-								</Td>
-							</Tr>
-						</Tbody>
-					</Table>
-					<Pagination />
+					{isLoading ? (
+						<Flex justify='center'>
+							<Spinner />
+						</Flex>
+					) : error ? (
+						<Flex justify='center'>
+							<Text>Falha ao tentar carregar os dados da tabela de usuários.</Text>
+						</Flex>
+					) : (
+						<>
+							<Table>
+								<Thead>
+									<Tr>
+										<Th px={['4', '4', '6']} color='gray.300' width='8'>
+											<Checkbox colorScheme='pink' />
+										</Th>
+										<Th>Usuário</Th>
+										<Th>Data de cadastro</Th>
+										<Th width='8'></Th>
+									</Tr>
+								</Thead>
+								<Tbody>
+									{data.map((user: IUsersView) => {
+										return (
+											<Tr key={user.id}>
+												<Td px={['4', '4', '6']}>
+													<Checkbox colorScheme='pink' />
+												</Td>
+												<Td>
+													<Box>
+														<Text fontWeight='bold'>{user.name}</Text>
+														<Text fontSize='sm' color='gray.300'>
+															{user.email}
+														</Text>
+													</Box>
+												</Td>
+												{isWideVersion && <Td>{user.createdAt}</Td>}
+												<Td>
+													<Button as='a' size='sm' fontSize='sm' colorScheme='purple' leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
+														Editar
+													</Button>
+												</Td>
+											</Tr>
+										);
+									})}
+								</Tbody>
+							</Table>
+							<Pagination />
+						</>
+					)}
 				</Box>
 			</Flex>
 		</Box>
